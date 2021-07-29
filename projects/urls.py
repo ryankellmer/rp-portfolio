50736309bd098faffcc8ef4391a04eae66e59fe0
ryankellmer <ryankellmer@gmail.com>
2021-07-29 14:01:00 -0700

    Commit 2

---
+++ projects/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path("", views.project_index, name="project_detail"),   # root URL is hooked up to the project_index view.
+        path("<int:pk>/", views.project_detail, name="project_detail"), # Value passed in the URL is an integer named pk.
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request):
+        projects = Project.objects.all()        # query: allows to create, retrieve, update, or delete objects in your database.
+        context = {     # Dictionary that is used to send information to my template.
+                'projects': projects            
+        }
+        return render(request, 'project_index.html', context)  # Entries added to the context are available in the template.
+        
+
+def project_detail(request, pk):
+        project = Project.objects.get(pk=pk)    # query retrieves the project with primary key.
+        context = {     # Dictionary gets assigned the project
+                'project': project
+        }
+        return render(request, 'project_detail.html', context)  # Project will pass to render(). 
