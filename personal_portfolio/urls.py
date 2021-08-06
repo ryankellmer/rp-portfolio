ab6e41701ff380a43dff0f0d27a32951874ca7f4
ryankellmer <ryankellmer@gmail.com>
2021-08-06 13:33:09 -0700

    Commit 7

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -21,6 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("projects.urls")),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
