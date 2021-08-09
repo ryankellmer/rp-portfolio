c16e7360dcb02e56accd5954a6b558634d6521bf
ryankellmer <ryankellmer@gmail.com>
2021-08-09 12:05:19 -0700

    Commit 14

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -129,9 +129,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
